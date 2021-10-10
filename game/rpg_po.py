d1974dd15cbd4031daa1e75832307c225054a2ce
Tio Lula <lulacode@gmail.com>
2021-10-10 17:25:46 -0300

    alteracao nos textos do console e timer de 3 segundos antes do resultado da batalha

--- game/rpg_po.py
+++ game/rpg_po.py
@@ -1,4 +1,5 @@
 import getpass
+import time
 
 def batalha(escolha_player1, escolha_player2):
     if escolha_player1=="ladino" and escolha_player2=="mago": return "mago"
@@ -13,10 +14,14 @@ def batalha(escolha_player1, escolha_player2):
 
 def main():
     print("player1,faça sua escolha! você vai à batalha como ladino, mago, ou guerreiro?")
-    escolha_player1 = getpass.getpass("player1:")
+    escolha_player1 = getpass.getpass("player1, digite sua escolha e aperte enter...")
     
     print("player2,faça sua escolha! você vai à batalha como ladino, mago, ou guerreiro?")
-    escolha_player2 = getpass.getpass("player2:")
+    escolha_player2 = getpass.getpass("player2, digite sua escolha e aperte enter...")
+    
+    print("E a batalha começa...")
+    print("QUE VENÇA O MELHOR!")
+    time.sleep(3)
 
     vencedor = batalha (escolha_player1, escolha_player2)
     if vencedor==escolha_player1: print("player1, você venceu essa grande e honrada batalha!!!")
