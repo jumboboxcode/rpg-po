e98d1566fc5240c392a783815d4fb3f0923b893e
Magrilo <magrilo888@gmail.com>
2021-10-11 21:26:57 -0300

    habilitaçao de empate (caso os 2 players escolham a mesma classe), com direito a historinha, ta ruim, mas ta legal.

--- game/rpg_po.py
+++ game/rpg_po.py
@@ -1,13 +1,17 @@
 import getpass
 import time
 
-def batalha(escolha_player1, escolha_player2):
-    if escolha_player1=="ladino" and escolha_player2=="mago": return "mago"
-    if escolha_player1=="ladino" and escolha_player2=="guerreiro": return "ladino"
-    if escolha_player1=="guerreiro" and escolha_player2=="ladino": return "ladino"
-    if escolha_player1=="mago" and escolha_player2=="ladino": return "mago"
-    if escolha_player1=="mago" and escolha_player2=="guerreiro": return "guerreiro"
-    if escolha_player1=="guerreiro" and escolha_player2=="mago": return "guerreiro"
+def batalha(escolha_player1, escolha_player2, ninguém):
+    if escolha_player1=="ladino" and escolha_player2=="mago" and ninguém=="nada": return "mago"
+    if escolha_player1=="ladino" and escolha_player2=="guerreiro" and ninguém=="nada": return "ladino"
+    if escolha_player1=="guerreiro" and escolha_player2=="ladino" and ninguém=="nada": return "ladino"
+    if escolha_player1=="mago" and escolha_player2=="ladino" and ninguém=="nada": return "mago"
+    if escolha_player1=="mago" and escolha_player2=="guerreiro" and ninguém=="nada": return "guerreiro"
+    if escolha_player1=="guerreiro" and escolha_player2=="mago" and ninguém=="nada": return "guerreiro"
+    if escolha_player1=="ladino" and escolha_player2=="ladino" and ninguém=="nada": return "nada"
+    if escolha_player1=="guerreiro" and escolha_player2=="guerreiro" and ninguém=="nada": return "nada"
+    if escolha_player1=="mago" and escolha_player2=="mago" and ninguém=="nada": return "nada"
+
 
 def main():
     print("player1,faça sua escolha! você vai à batalha como ladino, mago, ou guerreiro?")
@@ -16,14 +20,17 @@ def main():
     print("player2,faça sua escolha! você vai à batalha como ladino, mago, ou guerreiro?")
     escolha_player2 = getpass.getpass("player2, digite sua escolha e aperte enter...")
     
+    print("uma pessoa misteriosa dis que é ninguém e aposta no nada...")
+    ninguém = "nada"
+    
     print("E a batalha começa...")
     print("QUE VENÇA O MELHOR!")
     time.sleep(3)
 
-    vencedor = batalha (escolha_player1, escolha_player2)
+    vencedor = batalha (escolha_player1, escolha_player2, ninguém)
     if vencedor==escolha_player1: print("player1, você venceu essa grande e honrada batalha!!!")
     if vencedor==escolha_player2: print("player2, você venceu essa grande e honrada batalha!!!")
-    
+    if vencedor==ninguém: print ("a batalha foi muito disputada, ambos morreram, então, não é que aquele velho estava certo? ninguém foi considerado vencedor, nada... nada...")
 
 
 
--- tests/test_rpg_po.py
+++ tests/test_rpg_po.py
@@ -7,39 +7,65 @@ class TestRpgPo(TestCase):
     def teste_em_batalha_entre_ladino_e_guerreiro_ganha_o_ladino(self):
         escolha_player1 = "ladino"
         escolha_player2 = "guerreiro"
-        vencedor = rpgpo.batalha(escolha_player1, escolha_player2)
+        ninguém = "nada"
+        vencedor = rpgpo.batalha(escolha_player1, escolha_player2, ninguém)
         self.assertEqual(vencedor, escolha_player1)
 
 
     def teste_em_batalha_entre_ladino_e_mago_ganha_o_mago(self):
         escolha_player1 = "ladino"
         escolha_player2 = "mago"
-        vencedor = rpgpo.batalha(escolha_player1, escolha_player2) 
+        ninguém = "nada"
+        vencedor = rpgpo.batalha(escolha_player1, escolha_player2, ninguém) 
         self.assertEqual(vencedor, escolha_player2)
 
 
     def teste_em_batalha_entre_guerreiro_e_ladino_ganha_o_ladino(self):
         escolha_player1 = "guerreiro"
         escolha_player2 = "ladino"
-        vencedor = rpgpo.batalha(escolha_player1, escolha_player2)
+        ninguém = "nada"
+        vencedor = rpgpo.batalha(escolha_player1, escolha_player2, ninguém)
         self.assertEqual(vencedor, escolha_player2)
 
     def teste_em_batalha_entre_mago_e_ladino_ganha_o_mago(self):
         escolha_player1 = "mago"
         escolha_player2 = "ladino"
-        vencedor = rpgpo.batalha(escolha_player1, escolha_player2)
+        ninguém = "nada"
+        vencedor = rpgpo.batalha(escolha_player1, escolha_player2, ninguém)
         self.assertEqual(vencedor, escolha_player1)  
 
     def teste_em_batalha_entre_mago_e_guerreiro_ganha_o_guerreiro(self):
         escolha_player1 = "mago"
         escolha_player2 = "guerreiro"
-        vencedor = rpgpo.batalha(escolha_player1, escolha_player2)
+        ninguém = "nada"
+        vencedor = rpgpo.batalha(escolha_player1, escolha_player2, ninguém)
         self.assertEqual(vencedor, escolha_player2)
     
     def teste_em_batalha_entre_guerreiro_e_mago_ganha_o_guerreiro(self):
         escolha_player1 = "guerreiro"
         escolha_player2 = "mago"
-        vencedor = rpgpo.batalha(escolha_player1, escolha_player2)
+        ninguém = "nada"
+        vencedor = rpgpo.batalha(escolha_player1, escolha_player2, ninguém)
         self.assertEqual(vencedor, escolha_player1)
     
-    
+    def teste_em_batalha_ladino_entre_ladino_ganha_nada(self):
+        escolha_player1 = "ladino"
+        escolha_player2 = "ladino"
+        ninguém = "nada"
+        vencedor = rpgpo.batalha(escolha_player1, escolha_player2, ninguém)
+        self.assertEqual(vencedor, ninguém)
+
+    def teste_em_batalha_guerreiro_entre_guerreiro_ganha_nada(self):
+        escolha_player1 = "guerreiro"
+        escolha_player2 = "guerreiro"
+        ninguém = "nada"
+        vencedor = rpgpo.batalha(escolha_player1, escolha_player2, ninguém)
+        self.assertEqual(vencedor, ninguém)
+
+    def teste_em_batalha_mago_entre_mago_ganha_nada(self):
+        escolha_player1 = "mago"
+        escolha_player2 = "mago"
+        ninguém = "nada"
+        vencedor = rpgpo.batalha(escolha_player1, escolha_player2, ninguém)
+        self.assertEqual(vencedor, ninguém)
+
